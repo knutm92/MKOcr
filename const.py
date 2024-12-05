7d2f65af8d9498e48decb885d29d3b3e20c5a888
Michal Knut <knut.michal@gmail.com>
2024-12-05 08:56:14 +0100

    Refactored const.py

--- const.py
+++ const.py
@@ -1,12 +1,9 @@
 default_dpi = 450
 default_tesseract_path = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-
 threshold_block = 11
 threshold_c = 2
 noise_kernel = 3
-
-# File: tesseract_languages.py
 
 tesseract_languages = {
     "Afrikaans": "afr",
